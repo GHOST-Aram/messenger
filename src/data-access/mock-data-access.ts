0aec7c87c8535f5ad500ddf698f88807e76caa46
GHOST-Aram <owinoojala@gmail.com>
2024-02-24 10:39:59 +0300

    feat: Implement GET by message Id route handlers
          - Validate reference Id and handle validation errors
          - Use controller.getOne method to process the request

--- src/data-access/mock-data-access.ts
+++ src/data-access/mock-data-access.ts
@@ -1,5 +1,6 @@
 import { DataAccess } from "./data-access";
 import { HydratedMessageDoc, Message, MessageModel } from "./model";
+import { postData } from "../test/data/data";
 
 export class MockDataAccess extends DataAccess{
     constructor(model: MessageModel){
@@ -9,4 +10,12 @@ export class MockDataAccess extends DataAccess{
         ): Promise<HydratedMessageDoc>=>{
         return new Message(data)
     }) 
+
+    public findByReferenceId =  jest.fn(async(refId: string
+        ): Promise<HydratedMessageDoc| null> =>{
+        if(refId === '64c9e4f2df7cc072af2ac9e0')
+            return new Message(postData)
+
+        return null
+    })
 }
